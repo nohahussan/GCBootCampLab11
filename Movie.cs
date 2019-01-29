f94853b0596eca41efc4a9e4eca95fec90f39adc
Noha <noha.hussan@gmail.com>
2019-01-29 16:23:15 -0500

    submitt Lab11

---
+++ Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Lab_11
+{
+    public class Movie : IComparable<Movie>
+    {
+        private string title;
+        public string Title { get{return title; } }
+        private string category;
+        public string Category { get { return category; } }
+        private int categoryNum;
+        public int CategoryNum { get { return categoryNum; } }
+
+
+        public Movie(string title ,string category, int categoryNum)
+        {
+            this.title = title;
+            this.category = category;
+            this.categoryNum = categoryNum;
+
+        }
+
+        public int CompareTo(Movie that)
+        {
+            int result = this.Title.CompareTo(that.Title);
+            return result;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Lab_11
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welocome to the Movie List Application!");
+            List<Movie> movies = new List<Movie> ();
+
+            movies.Add(new Movie("Star Wars", "scifi",1));
+            movies.Add(new Movie("Bird Box", "scifi",1));
+
+            movies.Add(new Movie("Halloween", "horror",2));
+            movies.Add(new Movie("Hereditary", "horror",2));
+
+
+            movies.Add(new Movie("Christopher Robin", "drama",3));
+            movies.Add(new Movie("A Star Is Born", "drama",3));
+
+            movies.Add(new Movie("Incredibles 2", "animated",4));
+            movies.Add(new Movie("Ralph Breaks the Internet", "animated",4));
+
+
+            movies.Sort();//sort the arraylist
+
+            Boolean repeat = true;
+
+            while (repeat)
+            {
+                Console.WriteLine("There are {0} movies in this list.",movies.Count);
+                Console.WriteLine("What category are you interested in? (1.scifi,2.horror,3.drama,4.animated)");
+                var input = Console.ReadLine();
+                int choice;
+                if(int.TryParse(input,out choice))
+                { 
+                              foreach (Movie m in movies)
+                                {
+                                    if(choice == m.CategoryNum)
+                                        Console.WriteLine(m.Title);
+                                }
+
+                }
+                else if (input == "scifi" || input == "horror" || input == "drama" || input == "animated")
+                { 
+                    foreach(Movie m in movies)
+                    { 
+                        if(input == m.Category)
+                        {
+                            Console.WriteLine(m.Title);
+                        }
+                    }
+                }
+                else 
+                {
+                    Console.WriteLine("you entered unvalid choice please try again!!");
+
+                }
+                repeat = Continue();
+            }
+        }
+        public static Boolean Continue()
+        {
+            Console.WriteLine("Continue? (y/n) : ");
+            var input = Console.ReadLine().ToLower();
+            if (input == "y")
+            {
+                return true;
+            }
+            else if (input == "n")
+            {
+                return false;
+            }
+            else 
+            {
+                Console.WriteLine("You entered unvalid input!!");
+                return true;
+            }
+        }
+    }
+}
